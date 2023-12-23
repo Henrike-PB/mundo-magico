cf3415fc78cb391d4d3e7d6dbd585429df1e0d25
Henrike-PB <146462607+Henrike-PB@users.noreply.github.com>
2023-12-23 18:43:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const pre_carregamento = document.querySelector("div.pre-carregamento");
+
+
+function preCarregamento() {
+    pre_carregamento.style.opacity = "0";
+
+    setTimeout(() => {
+        pre_carregamento.style.display = "none";
+    }, 1000);
+}
